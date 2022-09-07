ce662990ebe8d7a773b5a2bc015b995774d74891
ChrisJPena <chrispclicks@gmail.com>
2022-09-07 00:10:58 -0400

    Created while statement to change Day Trip settings

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 import random
 
 # ""
-# (5 points): As a developer, I want to make at least three commits with descriptive messages.
+# DONE(5 points): As a developer, I want to make at least three commits with descriptive messages.
 
-# (5 points): As a developer, I want to store my destinations, restaurants, mode of transporation, and entertainments in theri own seperate lists. STRING
+# DONE(5 points): As a developer, I want to store my destinations, restaurants, mode of transporation, and entertainments in their own seperate lists. STRING
 
 # ex. "New York City", "San Francisco" atleast 4. no relation between the destinations, restaurants, etc. 
 
-# (5 points): As a user, I want a destination to be randomly selected for my day trip.
+# DONE(5 points): As a user, I want a destination to be randomly selected for my day trip.
 
-# (5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+# DONE(5 points): As a user, I want a restaurant to be randomly selected for my day trip.
 
-# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+# DONE(5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
 
-# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+# DONE(5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
 
 # (15 points): As a user, I want to be able to randomly re-select a destianation, restaurant, mode of transportation a form of entertainment if I don't like one or more of those things.
 
@@ -22,19 +22,51 @@ import random
 
 # x = ['x', 'x', 'x', 'x', 'x', 'X']
 
-destination = ['Bali', 'Dominican Republic', 'Iceland', 'Italy', 'Colombia', 'New Zealand']
-restaurant = ['Gekkō', 'Andy Tropical', 'MamaSushi', 'Genos Phillycheese steak', 'Olive Garden', 'Gekkō']
-mode_of_transportation = ['plane', 'train', 'car', 'teleportation', 'bicycle', 'uber']
-form_of_entertainment = ['Concert', 'Romantic Picnic', 'Museums', 'Beach', 'Theme Park', 'Spa']
+destination = ['Tampa', 'Miami', 'Orlando', 'Ft. Myers', 'Tallahassee', 'Jacksonville', 'Gainesville']
+restaurant = ['Gekkō', 'La Perrada', 'Berns Steakhouse', 'Mz.Chezious', 'Junior Colombia Burger', 'Los Chapos Tacos']
+mode_of_transportation = ['Plane', 'Train', 'Car', 'Streetcar', 'Bicycle', 'Uber']
+form_of_entertainment = ['Amalie Arena Concert', 'Romantic Picnic at Honeymoon Island', 'Museums in Downtown Tampa', 'Anna Maria Island Beach', 'Universal Studios', 'Wynwood']
 
 random_des = random.choice(destination)
 random_res = random.choice(restaurant)
 random_mot = random.choice(mode_of_transportation)
 random_foe = random.choice(form_of_entertainment)
 
-print(random_des)
-print(random_res)
-print(random_mot)
-print(random_foe)
+def day_trip_generator ():
+    print(f"Destination: {random_des}")
+    print(f"Restaurant: {random_res}")
+    print(f"Transportation: {random_mot}")
+    print(f"Entertainment: {random_foe}")
+
+day_trip_generator()
+travel_approval = input("Do you approve of your Day Trip?: (y/n) ")
+
+
+while travel_approval == 'n':
+    select_pop = input("Which Day Trip setting would you like to change? ")
+    
+    if select_pop == 'Destination':
+        random_des = random.choice(destination)
+        day_trip_generator()
+    
+    if select_pop == 'Restaurant':
+        random_des = random.choice(restaurant)
+        day_trip_generator()
+ 
+    if select_pop == 'Transportation':
+        random_des = random.choice(mode_of_transportation)
+        day_trip_generator()
+
+    if select_pop == 'Entertainment':
+        random_des = random.choice(form_of_entertainment)
+        day_trip_generator()
+    
+    travel_approval = input("Do you approve of your Day Trip?: (y/n) ")
+
+
+
+
+
+
 
 
